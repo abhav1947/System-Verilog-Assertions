b3e997eec3659ff540d6c6ec105a80b220c65309
abhav1947 <66078840+abhav1947@users.noreply.github.com>
2026-04-28 11:44:12 +0530

    Create sv_assertion2.sv

---
+++ sv_assertion2.sv
@@ -0,0 +1,29 @@
+module tb;
+  
+  bit a, b;
+  bit clk;
+  
+  always #10 clk = ~clk;
+  
+  initial begin
+    repeat(10) begin
+      @(posedge clk);
+      $monitor("clk = %0d", clk);
+      $monitor("a = %0d", a);
+      $monitor("b = %0d", b);
+      a = $urandom;
+      b = $urandom;
+    end
+      $finish();
+  end
+  
+initial begin
+  $dumpfile("dump.vcd"); // Name of the waveform file
+  $dumpvars(1);          // 1 = dump variables in the current module only
+  // Use $dumpvars(0, top_module_name); to dump all signals in the hierarchy
+end
+  assert property(
+    @(posedge clk)
+  	 a | b);
+    
+endmodule
