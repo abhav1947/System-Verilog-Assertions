642bf92a1ea5388dca7c68a3f4be0b8e477d55b3
abhav1947 <66078840+abhav1947@users.noreply.github.com>
2026-04-28 18:17:18 +0530

    Create sv_assertions_5.sv

---
+++ sv_assertions_5.sv
@@ -0,0 +1,39 @@
+/*
+Building Blocks of Assertions 
+
+Sequence: A sequence of multiple logical events typically form the functionality of any design. 
+
+Syntax:
+
+sequence <name_of_sequence>
+
+endsequence
+
+assert property(<name_of_sequence>);
+*/
+
+module tb;
+  
+  bit a,b;
+  bit clk;
+  
+  always #10 clk = ~clk;
+  
+  initial begin 
+    repeat(10) begin
+      @(posedge clk);
+      a = $urandom;
+      b = $urandom;
+    end
+    $finish;
+  end
+  
+  sequence a_and_b(bit a, bit b);
+    a && b;
+  endsequence
+  
+  assert property(
+    @(posedge clk)
+    a_and_b(.a(a), .b(b)));
+endmodule
+
