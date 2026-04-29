0be8c9e125a9e9063fa3f8438682dbe68fb08500
abhav1947 <66078840+abhav1947@users.noreply.github.com>
2026-04-29 17:21:27 +0530

    Create sv_assertions_$stable_15.sv

---
+++ sv_assertions_$stable_15.sv
@@ -0,0 +1,30 @@
+module tb;
+  
+  bit a, b;
+  bit rst, clk;
+  
+  always #10 clk = ~clk;
+  
+  initial begin
+    a = 0;
+    b = 0;
+    rst = 1;
+    #180;
+    rst = 0;
+    repeat(20) begin
+      a = $urandom;
+      b = $urandom;
+    end
+    $finish();
+  end
+  
+  initial begin
+  	$dumpfile("dump.vcd"); // Creates the dump file
+    $dumpvars(0, tb); // Dumps all variables in the specified module
+  end
+  
+  assert property(
+    @(posedge clk)
+    $stable(a) | $stable(b)) $display("PASS at t = %0t", $time); else $display("FAIL at t = %0t", $time);
+    
+endmodule
